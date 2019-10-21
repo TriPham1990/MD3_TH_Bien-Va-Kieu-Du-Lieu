fb82d384a61f09f0f4ea790aff62ad9f07576054
Tri Pham <pmtri@gmail.com>
2019-10-21 16:58:29 +0700

    update lan 1

--- thuchanh.ts
+++ thuchanh.ts
@@ -1,13 +1,24 @@
+// function main() {
+//     console.log("START");
+//     if (true) {
+//         var lang = "vi";
+//         var target = "en-us";
+//         console.log("inside block");
+//         console.log(target);
+//     }
+//     console.log(lang);
+//     console.log(target);
+// }
+
 function main() {
-    console.log("START");
-    if (true) {
-        var lang = "vi";
-        var target = "en-us";
-        console.log("inside block");
-        console.log(target);
-    }
-    console.log(lang);
-    console.log(target);
+    var x = 5;
+    console.log(x);
+    var x = 10;
+    console.log(x);
+    var y = 55;
+    console.log(y);
+    var y = 100;
+    console.log(y);
 }
 
 main();
